5e3ef465281a91f1c2a53000fb7709d3a623099e
pc <pc@DESKTOP-M7QJQNL>
2024-07-24 10:14:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Programın random belirlediği bir sayıya yaklaşan program.
+
+
+Random rnd = new Random();
+
+int randomSayi = rnd.Next(10);
+
+Console.WriteLine("Bakalım ne kadar şanşlısın? Random sayı belirlendi, BAŞLA!");
+
+int tahmin = Convert.ToInt32(Console.ReadLine());
+
+
+while (randomSayi != tahmin)
+{
+   
+    if (tahmin < randomSayi)
+    { 
+        Console.WriteLine("Daha büyük bir sayı gir \n"); 
+    }
+
+
+    else if (tahmin > randomSayi)
+    {
+        Console.WriteLine("Daha küçük bir sayı gir \n");
+    }
+    else
+    {
+        Console.WriteLine("BRAVO");
+    }
+}
+
+
+
